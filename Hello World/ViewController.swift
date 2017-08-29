d7e5df2c313dd04b64d902e26c32a31a8c752cdb
Jackson Chandler <actionjack94@gmail.com>
2017-08-29 10:59:40 -0500

    init

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Chandler, Jackson C. (MU-Student) on 8/29/17.
+//  Copyright © 2017 Chandler, Jackson C. (MU-Student). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
